d7732952a5d127ec9eb90dbe182ce970248d364a
Fazza Razaq Amiarso <fazzaamiarso@gmail.com>
2024-01-27 20:21:14 +0700

    chore: initialize and setup design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors : {
-"Marine blue": hsl(213, 96%, 18%)
-"Purplish blue": hsl(243, 100%, 62%)
-"Pastel blue": hsl(228, 100%, 84%)
-"Light blue": hsl(206, 94%, 87%)
-"Strawberry red": hsl(354, 84%, 57%)
-      }
+      fontFamily: {
+        ubuntu: ["ubuntu", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          marineBlue: "hsl(213, 96%, 18%)",
+          purplishBlue: "hsl(243, 100%, 62%)",
+          pastelBlue: "hsl(228, 100%, 84%)",
+          lightBlue: "hsl(206, 94%, 87%)",
+          strawberryRed: "hsl(354, 84%, 57%)",
+        },
+        neutral: {
+          coolGray: "hsl(231, 11%, 63%)",
+          lightGray: "hsl(229, 24%, 87%)",
+          magnolia: "hsl(217, 100%, 97%)",
+          alabaster: "hsl(231, 100%, 99%)",
+        },
+      },
     },
   },
   plugins: [],
